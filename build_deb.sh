aae03f49a9755ed976754d1d76ac71ada44bce6e
Allan Beaufour <allan@chartbeat.com>
2012-04-11 14:38:53 -0400

    fix file locations and bump version

--- build_deb.sh
+++ build_deb.sh
@@ -8,13 +8,9 @@ set -x
 # Package doozer
 mkdir -p doozer/usr/bin
 cp bin/doozer doozer/usr/bin
-mkdir -p doozer/DEBIAN
-cp doozer/control doozer/DEBIAN
 dpkg-deb --build doozer
 
 # Package doozerd
 mkdir -p doozerd/usr/bin
 cp bin/doozerd doozerd/usr/bin
-mkdir -p doozerd/DEBIAN
-cp doozerd/control doozerd/DEBIAN
 dpkg-deb --build doozerd
